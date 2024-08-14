b38e201aae1a71701e15aec205b0de50d5e2a96d
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-08-14 15:00:02 +0900

    Initial Commit

---
+++ Damoim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Damoim
+//
+//  Created by 조규연 on 8/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

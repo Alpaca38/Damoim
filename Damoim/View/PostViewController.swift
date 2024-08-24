180c604c9a2aee0977830b30daac22479343e478
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-08-24 16:15:50 +0900

    포스트 작성화면 UI 구현

--- Damoim/View/PostViewController.swift
+++ Damoim/View/PostViewController.swift
@@ -6,8 +6,110 @@
 //
 
 import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
 
 final class PostViewController: BaseViewController {
+    private let photoImage = {
+        let view = UIImageView()
+        view.image = UIImage(systemName: "camera.circle")?.withTintColor(.lightGray, renderingMode: .alwaysOriginal)
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    
+    private let photoTapGesture = UITapGestureRecognizer()
+    
+    private lazy var photoView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFill
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 10
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.lightGray.cgColor
+        view.addGestureRecognizer(photoTapGesture)
+        view.isUserInteractionEnabled = true
+        return view
+    }()
+    
+    private let titleTextField = {
+        let view = UITextField()
+        view.placeholder = l10nKey.placeholderTitle.rawValue.localized
+        view.borderStyle = .roundedRect
+        view.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+        view.leftViewMode = .always
+        return view
+    }()
+    
+    private let contentTextView = {
+        let view = UITextView()
+        view.layer.cornerRadius = 10
+        view.font = .systemFont(ofSize: 14)
+        view.textContainerInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        view.textColor = .lightGray
+        view.text = l10nKey.placeholderContent.rawValue.localized
+        return view
+    }()
+    
+    private let maxCountButton = {
+        let button = UIButton()
+        button.setTitle(l10nKey.buttonMaxCount.rawValue.localized, for: .normal)
+        button.backgroundColor = .lightGray
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
+    private let deadlineButton = {
+        let button = UIButton()
+        button.setTitle(l10nKey.buttonDeadline.rawValue.localized, for: .normal)
+        button.backgroundColor = .lightGray
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
+    private let priceLabel = {
+        let view = UILabel()
+        view.text = l10nKey.labelFee.rawValue.localized
+        return view
+    }()
+    
+    private let priceTextField = {
+        let view = UITextField()
+        view.placeholder = l10nKey.placeholderFee.rawValue.localized
+        view.keyboardType = .numberPad
+        return view
+    }()
+    
+    private lazy var contentView = {
+        let view = UIView()
+        view.addSubview(photoImage)
+        view.addSubview(photoView)
+        view.addSubview(titleTextField)
+        view.addSubview(contentTextView)
+        view.addSubview(maxCountButton)
+        view.addSubview(deadlineButton)
+        view.addSubview(priceLabel)
+        view.addSubview(priceTextField)
+        return view
+    }()
+    
+    private let scrollTapGesture = UITapGestureRecognizer()
+    
+    private lazy var scrollView = {
+        let view = UIScrollView()
+        view.addSubview(contentView)
+        view.addGestureRecognizer(scrollTapGesture)
+        return view
+    }()
+    
+    private let createPostButton = {
+        let button = UIButton()
+        button.setTitle(l10nKey.buttonPost.rawValue.localized, for: .normal)
+        button.backgroundColor = .lightGray
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
     private let viewModel: PostViewModel
     
     init(viewModel: PostViewModel) {
@@ -22,6 +124,77 @@ final class PostViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavi()
+        bind()
+    }
+    
+    override func configureLayout() {
+        view.addSubview(scrollView)
+        view.addSubview(createPostButton)
+        
+        photoView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(40)
+            $0.leading.equalToSuperview().offset(20)
+            $0.size.equalTo(80)
+        }
+        
+        photoImage.snp.makeConstraints {
+            $0.center.equalTo(photoView)
+            $0.size.equalTo(30)
+        }
+        
+        titleTextField.snp.makeConstraints {
+            $0.top.equalTo(photoView.snp.bottom).offset(20)
+            $0.leading.equalTo(photoView)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.height.equalTo(44)
+        }
+        
+        contentTextView.snp.makeConstraints {
+            $0.top.equalTo(titleTextField.snp.bottom).offset(20)
+            $0.horizontalEdges.equalTo(titleTextField)
+            $0.height.equalTo(200)
+        }
+        
+        maxCountButton.snp.makeConstraints {
+            $0.top.equalTo(contentTextView.snp.bottom).offset(20)
+            $0.horizontalEdges.equalTo(contentTextView)
+            $0.height.equalTo(44)
+        }
+        
+        deadlineButton.snp.makeConstraints {
+            $0.top.equalTo(maxCountButton.snp.bottom).offset(20)
+            $0.horizontalEdges.equalTo(contentTextView)
+            $0.height.equalTo(44)
+        }
+        
+        priceLabel.snp.makeConstraints {
+            $0.top.equalTo(deadlineButton.snp.bottom).offset(20)
+            $0.leading.equalTo(deadlineButton)
+        }
+        
+        priceTextField.snp.makeConstraints {
+            $0.top.equalTo(priceLabel.snp.bottom).offset(10)
+            $0.horizontalEdges.equalTo(contentTextView)
+            $0.height.equalTo(44)
+            $0.bottom.equalToSuperview().offset(-20)
+        }
+        
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+        
+        createPostButton.snp.makeConstraints {
+            $0.bottom.equalTo(view.keyboardLayoutGuide.snp.top).offset(-8)
+            $0.horizontalEdges.equalTo(priceTextField)
+            $0.height.equalTo(44)
+        }
+        
+        scrollView.snp.makeConstraints {
+            $0.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            $0.bottom.equalTo(createPostButton.snp.top)
+        }
+        
     }
 }
 
@@ -29,5 +202,33 @@ final class PostViewController: BaseViewController {
 private extension PostViewController {
     func setNavi() {
         navigationItem.title = l10nKey.navigationTitlePost.rawValue.localized
+    }
+}
+
+// MARK: Data bind
+private extension PostViewController {
+    func bind() {
+        disposeBag.insert {
+            contentTextView.rx.didBeginEditing
+                .bind(with: self) { owner, _ in
+                    if owner.contentTextView.textColor == .lightGray {
+                        owner.contentTextView.text = nil
+                        owner.contentTextView.textColor = .black
+                    }
+                }
+            
+            contentTextView.rx.didEndEditing
+                .bind(with: self) { owner, _ in
+                    if owner.contentTextView.text.isEmpty {
+                        owner.contentTextView.text = l10nKey.placeholderContent.rawValue.localized
+                        owner.contentTextView.textColor = .lightGray
+                    }
+                }
+            
+            scrollTapGesture.rx.event
+                .bind(with: self) { owner, _ in
+                    owner.view.endEditing(true)
+                }
+        }
     }
 }
